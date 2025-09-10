2ec43cf3c9bf56aecb5af8b2429bb692c44ac87f
Samuel <samuel.lorand@hotmail.com>
2025-09-10 20:57:20 -0300

    aula03

---
+++ aula03/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import RenderizandoComFuncoes from './components/RenderizandoComFuncoes'
+import TrabalhandoComImagens from './components/TrabalhandoComImagens'
+import HookContador from './components/HookContador'
+import EstouConseguindoAprenderReact from './components/EstouConseguindoAprenderReact'
+
+const App = () => {
+  return (
+    <div>
+      <EstouConseguindoAprenderReact estouConseguindo = {false}/>
+    </div>
+  )
+}
+
+export default App
---
+++ aula03/src/components/EstouComSono.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EstouComSono = (props) => {
+  return (
+    <div>{sono ? <h1>Hora de descansar</h1> : <h1>Bora estudar mais um pouco!</h1>}</div>
+  )
+}
+
+export default EstouComSono
